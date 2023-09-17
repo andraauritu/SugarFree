679c2e297b5ded7cf7bc7fbc535d98c081ad7e83
andraauritu <zuzulaptezuzulapte@yahoo.com>
2023-09-18 00:13:35 +0300

    repaired login

--- login.php
+++ login.php
@@ -21,7 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
       $_SESSION['user_id'] = $user['user_id'];
       $_SESSION['username'] = $user['username'];
       $_SESSION['role'] = $user['role'];
-      header('Location: homepage.php');
+      header('Location: index.php');
       exit();
     } else {
       $error = 'Invalid username or password.';
